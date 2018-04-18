7fa0e11aa4601cfc3000216a8f972290b310f398
Filipe Medeiros <fcma@cin.ufpe.br>
2018-04-17 21:15:19 -0300

    Consertando bug

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from autoslug import AutoSlugField
 
 from django.contrib.auth.models import User
@@ -26,3 +27,6 @@ class News (models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url (self):
+		return reverse('news:news', kwargs={'slug':self.slug})
