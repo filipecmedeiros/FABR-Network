7d31ba58c9331e8b60b758e517320635aba15e6e
Filipe Medeiros <fcma@cin.ufpe.br>
2018-04-17 21:05:25 -0300

    Adicionando leia também

--- news/views.py
+++ news/views.py
@@ -4,7 +4,7 @@ from django.views import generic
 from .models import News
 
 class NewsListView (generic.ListView):
-	queryset = News.objects.all()
+	queryset = News.objects.all().reverse()
 	model = News
 	template_name = 'new_list.html'
 	context_object_name = 'news'
@@ -13,7 +13,9 @@ news_list = NewsListView.as_view()
 
 def news (request, slug):
 	new = News.objects.get(slug=slug)
+	read_more = News.objects.all().reverse()[:3]
 	context={
 		'new' : new,
+		'read_more': read_more
 	}
 	return render (request, 'new.html', context)
