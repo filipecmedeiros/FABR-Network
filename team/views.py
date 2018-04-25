f8e3ccaa380029d43479a0c188288af2eb3302f6
Filipe Medeiros <fcma@cin.ufpe.br>
2018-04-25 00:59:23 -0300

    Formatando estilo de tabelas

--- team/views.py
+++ team/views.py
@@ -4,7 +4,6 @@ from .models import Team
 # Create your views here.
 def TeamView(request, slug):
 	context = {
-		'title': 'Teste',
 		'team' : Team.objects.get(slug=slug),
 	}
 	return render (request, 'roster.html', context)
