00c0e093a724e6176544ac1194b0f432651111b0
Filipe Medeiros <fcma@cin.ufpe.br>
2018-05-14 16:10:47 -0300

    Deploy na amazon

--- team/views.py
+++ team/views.py
@@ -4,52 +4,59 @@ from .models import Team, Player
 from datetime import datetime
 
 # Create your views here.
-class TeamListView(generic.DetailView):
-	model = Team
-	template_name = 'roster.html'
-	context_object_name = 'team'
 
-	def get_context_data(self, **kwargs):
-		context = super(TeamListView, self).get_context_data(**kwargs)
-		context['roster'] = Player.objects.all()
-		return context
+
+class TeamListView(generic.DetailView):
+    model = Team
+    template_name = 'roster.html'
+    context_object_name = 'team'
+
+    def get_context_data(self, **kwargs):
+        context = super(TeamListView, self).get_context_data(**kwargs)
+        context['roster'] = Player.objects.all()
+        return context
 
 TeamView = TeamListView.as_view()
 
+
 class ScheduleListView(generic.DetailView):
-	model = Team
-	template_name = 'schedule.html'
-	context_object_name = 'team'
+    model = Team
+    template_name = 'schedule.html'
+    context_object_name = 'team'
 
 ScheduleListView = ScheduleListView.as_view()
 
+
 class CompactListView(generic.DetailView):
-	model = Team
-	template_name='compact.html'
-	context_object_name = 'team'
+    model = Team
+    template_name = 'compact.html'
+    context_object_name = 'team'
 
 CompactListView = CompactListView.as_view()
 
+
 class HistoryDetailView(generic.DetailView):
-	model = Team
-	template_name = 'history.html'
+    model = Team
+    template_name = 'history.html'
 
 HistoryDetailView = HistoryDetailView.as_view()
 
+
 class CampaignDatailView(generic.DetailView):
-	model = Team
-	template_name = 'campaign.html'
+    model = Team
+    template_name = 'campaign.html'
 
 CampaignDatailView = CampaignDatailView.as_view()
 
-def player (request, slug, playerSlug):
-	team = Team.objects.get(slug=slug)
-	player =  Player.objects.get(slug=playerSlug)
-	idade = datetime.now().year - player.birthdate.year
-	context = {
-		'team': team,
-		'player': player,
-		'idade': idade,
-	}
 
-	return render (request, 'player.html', context)
+def player(request, slug, playerSlug):
+    team = Team.objects.get(slug=slug)
+    player = Player.objects.get(slug=playerSlug)
+    idade = datetime.now().year - player.birthdate.year
+    context = {
+        'team': team,
+        'player': player,
+        'idade': idade,
+    }
+
+    return render(request, 'player.html', context)
