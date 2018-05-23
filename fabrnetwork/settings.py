c5c56467fc6eae4220c72d4a47fb72e4d8c92e39
Filipe Medeiros <fcma@cin.ufpe.br>
2018-05-22 22:08:09 -0300

    Adicionando whitenoise ao heroku

--- fabrnetwork/settings.py
+++ fabrnetwork/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'fabrnetwork.urls'
@@ -126,12 +127,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #Static files heroku
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "core/static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 #Heroku settings
