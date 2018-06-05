b06332a3973ef679383b185d1cc83f058e0b2840
Filipe Medeiros <fcma@cin.ufpe.br>
2018-06-05 15:19:53 -0300

    Ajuste no cadastro do título de notícia (sugestão de 80 caracteres)

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from django.forms import TextInput
+from django.db import models
 
 # Register your models here.
 from .models import News
@@ -10,5 +12,9 @@ class NewsAdmin (SummernoteModelAdmin):
 	list_display = ['title', 'date', 'author', 'slug', 'created', 'modified']
 	search_display = ['title', 'date', 'author', 'slug', 'created', 'modified']
 	list_filter = ['date', 'author', 'created', 'modified']
+
+	formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'58'})},
+    }
 
 admin.site.register(News, NewsAdmin)
