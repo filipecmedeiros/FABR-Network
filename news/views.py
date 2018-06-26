6e408ed5c770f74435b55600b39cc2bb77c7b0f2
Filipe Medeiros <fcma@cin.ufpe.br>
2018-06-25 21:59:59 -0300

    Adicionando paginação a listagem de notícias

--- news/views.py
+++ news/views.py
@@ -8,6 +8,7 @@ class NewsListView (generic.ListView):
 	model = News
 	template_name = 'new_list.html'
 	context_object_name = 'news'
+	paginate_by = 10
 
 	def get_context_data(self, **kwargs):
 		context = super(NewsListView, self).get_context_data(**kwargs)
